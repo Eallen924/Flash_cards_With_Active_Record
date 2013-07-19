174ad803fc387a08e7470364838123a38823a4a2
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 16:26:35 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
-deck1 = Deck.new(:name => "States and Capitals")
-CSV.foreach('States.csv', :headers => true) do |row| 
-  Card.create(:term => row[0], :definition => row[2], :deck_id = deck1.id )
+require 'csv'
+
+deck1 = Deck.create(:name => "States and Capitals")
+
+CSV.foreach('StatesData/States.csv', :headers => true) do |row| 
+  Card.create(:term => row[2], :definition => row[0], :deck_id => deck1.id )
 end
