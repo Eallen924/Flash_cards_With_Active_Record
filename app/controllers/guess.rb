f89b516f8254dd61697d4f51b2f2d65c9a776dc3
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 17:25:12 -0500

    added round views and routes

--- app/controllers/guess.rb
+++ app/controllers/guess.rb
@@ -1,9 +1,11 @@
 get '/deck/:deck_id/round/:round_id/card/:card_id/guess/:guess_id' do
+  
   if params[:guess][:term] == params[:guess][:user_guess]
-    session[:card_ids].select
+    @guess = Guess.create(card_id: :card_id, correct?: 1, user_guess: params[:guess][:user_guess])
+    session[:card_ids].delete_if {|card| card.card_id == :card_id}
+    erb :answer
   else
-    #giv
+    @guess = Guess.create(card_id: :card_id, correct?: 0, user_guess: params[:guess][:user_guess])
+    erb :answer
   end
- @card = Card.find(session[:card_ids].sample)
-
 end
