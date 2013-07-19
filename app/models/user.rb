a289c2bd7e22a92759ec30e03854a583e8a0e6e2
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 14:44:17 -0500

    add bcrypt user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,20 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: => true
   validates :email, presence: true, uniqueness: => true
   validates :password, presence: true,length: { in: 6..20 }
+  validates :password_hash, presence: => true
 
   
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
 
-  def self.authenticate(email, password)
-    user = self.find_by_email(email)
+  def password=(new_password)
+    @password = Bcrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(username, password)
+    user = self.find_by_username(username)
     if user && user.password == password
       user
     else
