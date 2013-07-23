0c8b8c7c7a822c2ec6add11ad24012f6e33e0668
Eric Allen <eric.justin.allen@gmail.com>
2013-07-22 19:15:56 -0500

    fix ajax, fully functional

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
-deck1 = Deck.create(:name => "States and Capitals Easy")
+deck1 = Deck.create(:name => "States and Capitals")
 
-CSV.foreach('StatesData/States1.csv', :headers => true) do |row| 
+CSV.foreach('StatesData/States.csv', :headers => true) do |row| 
   Card.create(:term => row[2], :definition => row[0], :deck_id => deck1.id )
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,24 +1,26 @@
-// var dataTransition = function(data){
-//   $('#container').replaceWith(data);
-// };
+var dataTransition = function(data){
+  console.log(data);
+  $('#container').replaceWith(data);
+};
 
-// $(document).ready(function() {
-//   $('#start_game').on('click', function(event){
-//     event.preventDefault();
-//     var url = $(this).attr('href');
-//     $.get(url, dataTransition);
-//   });
+$(document).ready(function() {
+  $('.start_game').on('click', function(event){
+    event.preventDefault();
+    var url = $(this).attr('href');
+    $.get(url, dataTransition);
+  });
 
-//   $('#card_question').on('submit', function(event) {
-//     event.preventDefault();
-//     var url = $(this).attr('action');
-//     var userGuess = $(this).serialize();
-//     $.post(url, userGuess, dataTransition);
-//   });
+  $('body').on('submit', '#card_question', function(event) {
+    event.preventDefault();
+    console.log(this);
+    var url = $(this).attr('action');
+    var userGuess = $(this).serialize();
+    $.post(url, userGuess, dataTransition);
+  });
 
-//   $('#next_card').on('click', function(event){
-//     event.preventDefault();
-//     var url = $(this).attr('href');
-//     $.get(url, dataTransition);
-//   });
-// });
+  $('body').on('click','#next_card', function(event){
+    event.preventDefault();
+    var url = $(this).attr('href');
+    $.get(url, dataTransition);
+  });
+});
